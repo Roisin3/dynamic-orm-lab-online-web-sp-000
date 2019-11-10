d8f0e91f7ea98d73397c6edf65c35463c9fb61f3
Michael Lynch <michael@michaelrlynch.com>
2019-11-10 04:37:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end
 
   def seld.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
